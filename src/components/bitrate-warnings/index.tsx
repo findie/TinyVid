a13a7b82e9dbbd8a21135a0b9ad5e7f82b1a8174
Stefan-Gabriel Muscalu <stefan.gabriel.muscalu@gmail.com>
2020-06-19 17:20:17 +0300

    update: refactored alerts & support temp suppression

--- src/components/bitrate-warnings/index.tsx
+++ src/components/bitrate-warnings/index.tsx
@@ -1,10 +1,79 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {DetailsComms} from "../../helpers/comms";
 import {VideoSettings} from "../../../electron/types";
 import {Collapse} from "@material-ui/core";
-import {Alert, AlertTitle} from "@material-ui/lab"
+import {Alert, AlertProps, AlertTitle} from "@material-ui/lab"
 import {FFHelpers} from "../../../electron/helpers/ff";
 import {between} from "../../helpers/math";
+
+type AlertVariants = 'corrupt' | 'bad' | 'artifacts' | 'ok' | 'waste';
+
+const alertMessages: ({ [s in AlertVariants]: { title: string, text: string, severity: AlertProps['severity'], type: AlertVariants } }) = {
+  corrupt: {
+    title: 'Your video will corrupt!',
+    text: 'Reduce video duration, fps, resolution or increase file size.',
+    severity: "error",
+    type: "corrupt"
+  },
+  bad: {
+    title: 'Video may look blocky!',
+    text: 'Depending on how much movement there is, you may have to reduce video duration, fps, resolution or increase file size.',
+    severity: "warning",
+    type: 'bad',
+  },
+  artifacts: {
+    title: 'Some artifacts may occur!',
+    text: 'Depending on how much movement there is, you may experience some visual quality loss.',
+    severity: 'warning',
+    type: "artifacts"
+  },
+  ok: {
+    title: 'All looks to be Ok!',
+    text: '',
+    severity: 'success',
+    type: "ok"
+  },
+  waste: {
+    title: 'You may be wasting space!',
+    text: 'It is possible to lower the file size or increase resolution or fps without losses.',
+    severity: 'warning',
+    type: "waste"
+  }
+}
+
+function getAlertType(videoDetails: DetailsComms.SimpleVideoDetails, videoSettings: VideoSettings, fileSizeInBytes: number, duration: number) {
+  const bitrateThresholds = FFHelpers.optimalBitrateCalculator(videoDetails, videoSettings);
+  const averageBitrateInKb = FFHelpers.computeAverageBPS(fileSizeInBytes, duration);
+
+  const averageVideoBitrateInBits = averageBitrateInKb.videoBitrateInKb * 1024;
+
+  // bad quality
+  if (between(bitrateThresholds.mayCorrupt[0], averageVideoBitrateInBits, bitrateThresholds.mayCorrupt[1])) {
+    return alertMessages.corrupt;
+  }
+  if (between(bitrateThresholds.veryBad[0], averageVideoBitrateInBits, bitrateThresholds.veryBad[1])) {
+    return alertMessages.bad;
+  }
+  if (between(bitrateThresholds.blockingArtifacts[0], averageVideoBitrateInBits, bitrateThresholds.blockingArtifacts[1])) {
+    return alertMessages.artifacts;
+  }
+
+  // ok
+  if (
+    between(bitrateThresholds.good[0], averageVideoBitrateInBits, bitrateThresholds.good[1]) ||
+    between(bitrateThresholds.diminishingReturns[0], averageVideoBitrateInBits, bitrateThresholds.diminishingReturns[1])
+  ) {
+    return alertMessages.ok;
+  }
+
+  // too big
+  if (between(bitrateThresholds.wastedSpace[0], averageVideoBitrateInBits, bitrateThresholds.wastedSpace[1])) {
+    return alertMessages.waste;
+  }
+
+  console.trace('you should not be here');
+  return alertMessages.ok;
+}
 
 export interface BitrateWarningsProps {
   videoDetails: DetailsComms.SimpleVideoDetails | null,
@@ -20,73 +89,35 @@ export function BitrateWarnings(props: BitrateWarningsProps) {
     return null;
   }
 
-  const bitrateThresholds = FFHelpers.optimalBitrateCalculator(props.videoDetails, props.videoSettings);
-  const averageBitrateInKb = FFHelpers.computeAverageBPS(props.fileSizeInBytes, props.duration);
-  const averageVideoBitrateInBits = averageBitrateInKb.videoBitrateInKb * 1024;
-  let alert: React.ReactNode = null;
-  let shouldShowAlert = false;
+  const [suppress, setSuppress] = useState(false);
+  const [lastAlert, setLastAlert] = useState<AlertVariants>("ok");
+  const alertData = getAlertType(props.videoDetails, props.videoSettings, props.fileSizeInBytes, props.duration);
 
-  // console.log('avg bitrate in Kb', averageBitrateInKb.videoBitrateInKb)
-  // console.log(bitrateThresholds)
-  // console.log('bits per pixel ratio', averageVideoBitrateInBits / (props.videoDetails.width * props.videoDetails.height * props.videoDetails.fps))
-  // console.log('~'.repeat(80));
-
-  // bad quality
-  if (between(bitrateThresholds.mayCorrupt[0], averageVideoBitrateInBits, bitrateThresholds.mayCorrupt[1])) {
-    alert =
-      <Alert elevation={6} severity={'error'} variant={'filled'}>
-        <AlertTitle>Your video will corrupt!</AlertTitle>
-        Reduce video duration, fps, resolution or increase file size.
-      </Alert>;
-
-    shouldShowAlert = true;
-  }
-  if (between(bitrateThresholds.veryBad[0], averageVideoBitrateInBits, bitrateThresholds.veryBad[1])) {
-    alert =
-      <Alert elevation={6} severity={'warning'} variant={'filled'}>
-        <AlertTitle>Video may look blocky!</AlertTitle>
-        Depending on how much movement there is, you may have to reduce video duration, fps, resolution or increase file
-        size.
-      </Alert>;
-    shouldShowAlert = true;
-
-  }
-  if (between(bitrateThresholds.blockingArtifacts[0], averageVideoBitrateInBits, bitrateThresholds.blockingArtifacts[1])) {
-    alert =
-      <Alert elevation={6} severity={'warning'} variant={'filled'}>
-        <AlertTitle>Some artifacts may occur!</AlertTitle>
-        Depending on how much movement there is, you may experience some visual quality loss.
-      </Alert>;
-    shouldShowAlert = true;
-
+  function temp_close_alert() {
+    console.log('should suppress alert')
+    setSuppress(true);
   }
 
-  // ok
-  if (
-    between(bitrateThresholds.good[0], averageVideoBitrateInBits, bitrateThresholds.good[1]) ||
-    between(bitrateThresholds.diminishingReturns[0], averageVideoBitrateInBits, bitrateThresholds.diminishingReturns[1])
-  ) {
-    alert =
-      <Alert elevation={6} severity={'success'} variant={'filled'}>
-        <AlertTitle>All looks to be Ok!</AlertTitle>
-      </Alert>;
-  }
+  useEffect(() => {
 
-  // too big
-  if (between(bitrateThresholds.wastedSpace[0], averageVideoBitrateInBits, bitrateThresholds.wastedSpace[1])) {
-    alert =
-      <Alert elevation={6} severity={'warning'} variant={'filled'}>
-        <AlertTitle>You may be wasting space!</AlertTitle>
-        It is possible to lower the file size or increase resolution or fps without losses.
-      </Alert>;
-    shouldShowAlert = true;
+    if (lastAlert !== alertData.type) {
+      setLastAlert(alertData.type);
+      setSuppress(false);
+    }
 
+  }, [props])
+
+  if (suppress) {
+    return null;
   }
 
   return (
     <div className={props.className}>
-      <Collapse in={shouldShowAlert} timeout={{ exit: 3000 }}>
-        {alert}
+      <Collapse in={alertData.type !== 'ok'} timeout={{ exit: 3000 }}>
+        <Alert elevation={6} severity={alertData.severity} variant={'standard'} onClose={temp_close_alert}>
+          <AlertTitle>{alertData.title}</AlertTitle>
+          {alertData.text}
+        </Alert>
         <div style={{ height: '100px' }}>
           {/* add some extra height to act as a delay for the collapse to exit */}
         </div>
