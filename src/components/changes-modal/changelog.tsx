c2dc1f02ae2c6a0231e400b82814d283025a11e5
Stefan-Gabriel Muscalu <stefan.gabriel.muscalu@gmail.com>
2021-04-19 20:03:38 +0300

    fix: more headway fixes

--- src/components/changes-modal/changelog.tsx
+++ src/components/changes-modal/changelog.tsx
@@ -5,11 +5,27 @@ import React, {useEffect, useState} from "react";
 import * as css from './styles.css'
 import {eventList} from "../../helpers/events";
 import {Link} from "@material-ui/core";
+import classNames from "classnames";
 
 declare global {
   interface Window {
     Headway: any,
   }
+}
+
+async function headwayReady() {
+  for (let i = 0; i < 10; i++) {
+    console.log('checking headway...', i);
+    if (window.Headway) {
+      console.log('found headway!');
+      console.log('will return true')
+      return true;
+    } else {
+      console.log('headway not found...');
+      await new Promise(_ => setTimeout(_, 1000));
+    }
+  }
+  return false;
 }
 
 async function initHeadway() {
@@ -35,38 +51,42 @@ async function initHeadway() {
 
   console.log('initting headway with', config);
 
-  let inited = false;
-
-  for (let i = 0; i < 10; i++) {
-    if (window.Headway) {
-      try {
-        window.Headway.init(config);
-        inited = true;
-        break;
-      } catch (e) {
-        console.error(e);
-        await new Promise(_ => setTimeout(_, 1000));
-      }
-    } else {
-      await new Promise(_ => setTimeout(_, 1000));
+  for (let i = 0; i < 3; i++) {
+    try {
+      const hw = window.Headway.init(config);
+      console.log('headway inited',hw);
+      return hw;
+    } catch (e) {
+      console.error(e);
+      await new Promise(_ => setTimeout(_, 500));
     }
   }
-
-  return inited;
+  return null;
 }
 
 export function Changelog() {
 
+  const [isLoaded, setIsLoaded] = useState(false);
   const [isInited, setIsInited] = useState(false);
 
   useEffect(() => {
-    initHeadway().then(setIsInited).catch(console.error);
-  }, [setIsInited]);
-
-  if (!isInited) return null;
+    headwayReady()
+      .then((found) => {
+        console.log('headway found', found);
+        setIsLoaded(found);
+        if (found) {
+          return initHeadway();
+        }
+        return null;
+      })
+      .then(headway => setIsInited(!!headway))
+      .catch(console.error);
+  }, [setIsLoaded, setIsInited]);
 
   return (
-    <Link className={css.text}>
+    <Link
+      className={classNames(css.text, (!isLoaded || !isInited) && css.hidden)}
+    >
       View latest changes
     </Link>
   )
