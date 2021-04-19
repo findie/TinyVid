ac62571fc7b10d37bcfd38973e47527681b7dd53
Stefan-Gabriel Muscalu <stefan.gabriel.muscalu@gmail.com>
2021-04-19 11:17:59 +0300

    update: add discord nitro classic / discord nitro difference

--- src/config/max-file-size/index.tsx
+++ src/config/max-file-size/index.tsx
@@ -37,9 +37,9 @@ export function ConfigMaxFileSize(props: ConfigMaxFileSizeProps) {
       >
         <MenuItem value={8}> 8 MB (Discord Free)</MenuItem>
         <MenuItem value={10}>10 MB</MenuItem>
-        <MenuItem value={50}>50 MB (Discord Nitro)</MenuItem>
+        <MenuItem value={50}>50 MB (Discord Nitro Classic)</MenuItem>
         <MenuItem value={64}>64 MB (WhatsApp)</MenuItem>
-        <MenuItem value={100}>100 MB</MenuItem>
+        <MenuItem value={100}>100 MB (Discord Nitro)</MenuItem>
         <MenuItem value={'custom'}>Custom</MenuItem>
       </Select>
     </FormControl>
