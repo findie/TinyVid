68928356792d33c67334e74f4768fab855f92d5b
Stefan-Gabriel Muscalu <stefan.gabriel.muscalu@gmail.com>
2021-04-20 09:54:59 +0300

    fix: code component can select text

--- src/components/code/index.tsx
+++ src/components/code/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {Box} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
-import {Theme} from "../../helpers/theme";
 import color from 'color'
 import * as css from './style.css'
 
